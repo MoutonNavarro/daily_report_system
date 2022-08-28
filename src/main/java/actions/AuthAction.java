20a39d17f6be029e537bafe1df2836e9abe9121c
Mouton.Navarro <Denmaniraven@yahoo.co.jp>
2022-08-28 19:48:54 +0900

    Create login action

--- src/main/java/actions/AuthAction.java
+++ src/main/java/actions/AuthAction.java
@@ -4,8 +4,11 @@ import java.io.IOException;
 
 import javax.servlet.ServletException;
 
+import actions.views.EmployeeView;
 import constants.AttributeConst;
 import constants.ForwardConst;
+import constants.MessageConst;
+import constants.PropertyConst;
 import services.EmployeeService;
 
 /**
@@ -48,6 +51,50 @@ public class AuthAction extends ActionBase {
 
         //Show login screen
         forward(ForwardConst.FW_LOGIN);
+    }
+
+    /**
+     * Do process login
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void login() throws ServletException, IOException{
+
+        String code = getRequestParam(AttributeConst.EMP_CODE);
+        String plainPass = getRequestParam(AttributeConst.EMP_PASS);
+        String pepper = getContextScope(PropertyConst.PEPPER);
+
+        //Authenticate valid employee
+        Boolean isValidEmployee = service.validateLogin(code, plainPass, pepper);
+
+        if (isValidEmployee) {
+            //In case authentication successful
+
+            //Anti-CSRF check token
+            if (checkToken()) {
+
+                //Acquire DB data of the logged in employee
+                EmployeeView ev = service.findOne(code, plainPass, pepper);
+                //Set logged in employee at session
+                putSessionScope(AttributeConst.LOGIN_EMP, ev);
+                //Set flush message about logged in completely at session
+                putSessionScope(AttributeConst.FLUSH, MessageConst.I_LOGINED.getMessage());
+                //Redirect to top page
+                redirect(ForwardConst.ACT_TOP, ForwardConst.CMD_INDEX);
+            }
+        }else {
+            //In case authentication failure
+
+            //Set token for anti-CSRF
+            putRequestScope(AttributeConst.TOKEN, getTokenId());
+            //Set show authentication failure error message
+            putRequestScope(AttributeConst.LOGIN_ERR, true);
+            //Set inputed employee code
+            putRequestScope(AttributeConst.EMP_CODE, code);
+
+            //Show login screen
+            forward(ForwardConst.FW_LOGIN);
+        }
     }
 
 }
