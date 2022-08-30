51924a22b1ee41a258f70ab0839d0a669d0e5405
Mouton.Navarro <Denmaniraven@yahoo.co.jp>
2022-08-30 17:46:41 +0900

    Create daily report index action

---
+++ src/main/java/actions/ReportAction.java
@@ -0,0 +1,272 @@
+package actions;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+
+import actions.views.ReportView;
+import constants.AttributeConst;
+import constants.ForwardConst;
+import constants.JpaConst;
+import services.ReportService;
+
+/**
+ * Action class that processing relation to daily report
+ *
+ */
+public class ReportAction extends ActionBase {
+
+    private ReportService service;
+
+    /**
+     * Do method
+     */
+    @Override
+    public void process() throws ServletException, IOException {
+
+        service = new ReportService();
+
+        //Run method
+        invoke();
+
+        service.close();
+    }
+
+    /**
+     * Show list screen
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void index() throws ServletException, IOException{
+        //Acquire report data for show list screen that pointed number of page
+        int page = getPage();
+        List<ReportView> reports = service.getAllPerPage(page);
+
+        //Acquire all number of report data
+        long reportCount = service.countAll();
+
+        putRequestScope(AttributeConst.REPORTS, reports); //Acquired report data
+        putRequestScope(AttributeConst.REP_COUNT, reportCount); //Number of all report data
+        putRequestScope(AttributeConst.PAGE, page); //Number of page
+        putRequestScope(AttributeConst.MAX_ROW, JpaConst.ROW_PER_PAGE); //Number of record that showing at 1 page
+
+        //In case set flush message at session then move to request scope and remove from session
+        String flush = getSessionScope(AttributeConst.FLUSH);
+        if (flush != null) {
+            putRequestScope(AttributeConst.FLUSH, flush);
+            removeSessionScope(AttributeConst.FLUSH);
+        }
+
+        //show list screen
+        forward(ForwardConst.FW_REP_INDEX);
+
+    }
+
+//
+//    /**
+//     * Show new registration screen
+//     * @throws ServletException
+//     * @throws IOException
+//     */
+//    public void entryNew() throws ServletException, IOException{
+//
+//        //Check administrator flag
+//        if(checkAdmin()) {
+//            putRequestScope(AttributeConst.TOKEN, getTokenId()); //Token for anti-CSRF
+//            putRequestScope(AttributeConst.EMPLOYEE, new EmployeeView()); //Empty employee instance
+//
+//            //Show new registration screen
+//            forward(ForwardConst.FW_EMP_NEW);
+//        }
+//    }
+//
+//    /**
+//     * Do new registration
+//     * @throws ServletException
+//     * @throws IOException
+//     */
+//    public void create() throws ServletException, IOException{
+//
+//        //Check administrator flag and check token for anti-CSRF
+//        if(checkAdmin() && checkToken()) {
+//
+//            //Create instance of employee information from parameter value
+//            EmployeeView ev = new EmployeeView(
+//                    null,
+//                    getRequestParam(AttributeConst.EMP_CODE),
+//                    getRequestParam(AttributeConst.EMP_NAME),
+//                    getRequestParam(AttributeConst.EMP_PASS),
+//                    toNumber(getRequestParam(AttributeConst.EMP_ADMIN_FLG)),
+//                    null,
+//                    null,
+//                    AttributeConst.DEL_FLAG_FALSE.getIntegerValue());
+//
+//            //Acquire pepper character string from application scope
+//            String pepper = getContextScope(PropertyConst.PEPPER);
+//
+//            //Employee information
+//            List<String> errors = service.create(ev, pepper);
+//
+//            if (errors.size() > 0) {
+//                //In case error happened
+//
+//                putRequestScope(AttributeConst.TOKEN, getTokenId()); //The token for anti-CSRF
+//                putRequestScope(AttributeConst.EMPLOYEE, ev); //Inputed employee information
+//                putRequestScope(AttributeConst.ERR, errors); //List of errors
+//
+//                //Re-show new registration screen
+//                forward(ForwardConst.FW_EMP_NEW);
+//
+//            }else {
+//                //In case no errors until registering
+//
+//                //Set flush message about register complete at the session
+//                putSessionScope(AttributeConst.FLUSH, MessageConst.I_REGISTERD.getMessage());
+//
+//                //Redirect to list screen
+//                redirect(ForwardConst.ACT_EMP, ForwardConst.CMD_INDEX);
+//            }
+//        }
+//    }
+//
+//    /**
+//     * Show detail screen
+//     * @throws ServletException
+//     * @throws IOException
+//     */
+//    public void show() throws ServletException, IOException{
+//
+//        //Check administrator flag
+//        if(checkAdmin()) {
+//            //Acquire employee data with ID as a condition
+//            EmployeeView ev = service.findOne(toNumber(getRequestParam(AttributeConst.EMP_ID)));
+//
+//            if (ev == null || ev.getDeleteFlag() == AttributeConst.DEL_FLAG_TRUE.getIntegerValue()) {
+//
+//                //In case failure to acquire a data or it has logical deleted then show error screen
+//                forward(ForwardConst.FW_ERR_UNKNOWN);
+//                return;
+//            }
+//
+//            putRequestScope(AttributeConst.EMPLOYEE, ev); //Acquired employee information
+//
+//            //Show detail screen
+//            forward(ForwardConst.FW_EMP_SHOW);
+//        }
+//    }
+//
+//    /**
+//     * Show edit screen
+//     * @throws ServletException
+//     * @throws IOException
+//     */
+//    public void edit() throws ServletException, IOException{
+//
+//        //Check administrator flag
+//        if(checkAdmin()) {
+//           //Acquire employee data with ID as a condition
+//            EmployeeView ev = service.findOne(toNumber(getRequestParam(AttributeConst.EMP_ID)));
+//
+//            if (ev ==null || ev.getDeleteFlag() == AttributeConst.DEL_FLAG_TRUE.getIntegerValue()) {
+//
+//                //In case failure to acquire the data or that has been logical deleted then show error screen.
+//                forward(ForwardConst.FW_ERR_UNKNOWN);
+//                return;
+//            }
+//
+//            putRequestScope(AttributeConst.TOKEN, getTokenId()); //Token for anti-CSRF
+//            putRequestScope(AttributeConst.EMPLOYEE, ev); //Acquired employee information
+//
+//            //Show edit screen
+//            forward(ForwardConst.FW_EMP_EDIT);
+//        }
+//    }
+//    /**
+//     * Do update
+//     * @throws ServletException
+//     * @throws IOException
+//     */
+//    public void update() throws ServletException, IOException {
+//
+//        //Check administrator flag and check token for anti-CSRF
+//        if(checkAdmin() && checkToken()) {
+//            //Create instance of employee information from value of parameter
+//            EmployeeView ev = new EmployeeView(
+//                    toNumber(getRequestParam(AttributeConst.EMP_ID)),
+//                    getRequestParam(AttributeConst.EMP_CODE),
+//                    getRequestParam(AttributeConst.EMP_NAME),
+//                    getRequestParam(AttributeConst.EMP_PASS),
+//                    toNumber(getRequestParam(AttributeConst.EMP_ADMIN_FLG)),
+//                    null,
+//                    null,
+//                    AttributeConst.DEL_FLAG_FALSE.getIntegerValue());
+//
+//            //Acquire pepper character string from application scope
+//            String pepper = getContextScope(PropertyConst.PEPPER);
+//
+//            //Update employee information
+//            List<String> errors = service.update(ev,  pepper);
+//
+//            if (errors.size() > 0) {
+//                //Errors happened until updating
+//
+//                putRequestScope(AttributeConst.TOKEN, getTokenId()); //Token for anti-CSRF
+//                putRequestScope(AttributeConst.EMPLOYEE, ev); //Inputed employee information
+//                putRequestScope(AttributeConst.ERR, errors); //List of errors
+//
+//                //Re-show edit screen
+//                forward(ForwardConst.FW_EMP_EDIT);
+//            }else {
+//                //In case no errors happened until updating
+//
+//                //Set flush message about update complete at session
+//                putSessionScope(AttributeConst.FLUSH, MessageConst.I_UPDATED.getMessage());
+//
+//                //Redirect to list screen
+//                redirect(ForwardConst.ACT_EMP, ForwardConst.CMD_INDEX);
+//            }
+//        }
+//    }
+//
+//    /**
+//     * Do logical delete
+//     * @throws ServletException
+//     * @throws IOException
+//     */
+//    public void destroy() throws ServletException, IOException{
+//        //Check administrator flag and check token for anti-CSRF
+//        if(checkAdmin() && checkToken()) {
+//            //Logical delete employee data with ID as a condition
+//            service.destroy(toNumber(getRequestParam(AttributeConst.EMP_ID)));
+//
+//            //Set flush message about delete complete at session
+//            putSessionScope(AttributeConst.FLUSH, MessageConst.I_DELETED.getMessage());
+//
+//            //Redirect to list screen
+//            redirect(ForwardConst.ACT_EMP, ForwardConst.CMD_INDEX);
+//        }
+//    }
+//    /**
+//     * Check logged in employee is administrator and when it is not administrator show error screen
+//     * true: Administrator, false: not administrator
+//     * @throws ServletException
+//     * @throws IOException
+//     */
+//    private boolean checkAdmin() throws ServletException, IOException{
+//
+//        //Acquire logged in employee information from session
+//        EmployeeView ev = (EmployeeView) getSessionScope(AttributeConst.LOGIN_EMP);
+//
+//        //If not administrator show error screen
+//        if (ev.getAdminFlag() != AttributeConst.ROLE_ADMIN.getIntegerValue()) {
+//
+//            forward(ForwardConst.FW_ERR_UNKNOWN);
+//            return false;
+//
+//        }else {
+//            return true;
+//        }
+//    }
+
+}
---
+++ src/main/webapp/WEB-INF/views/reports/index.jsp
@@ -0,0 +1,55 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<%@ page import="constants.ForwardConst" %>
+
+<c:set var="actRep" value="${ForwardConst.ACT_REP.getValue()}" />
+<c:set var="commShow" value="${ForwardConst.CMD_SHOW.getValue()}" />
+<c:set var="commNew" value="${ForwardConst.CMD_NEW.getValue()}" />
+<c:set var="commIdx" value="${ForwardConst.CMD_INDEX.getValue()}" />
+
+<c:import url="/WEB-INF/views/layout/app.jsp">
+    <c:param name="content">
+        <c:if test="${flush != null}">
+            <div id="flush_success">
+                <c:out value="${flush}"></c:out>
+            </div>
+        </c:if>
+        <h2>Daily report list</h2>
+        <table id="report_list">
+            <tbody>
+                <tr>
+                    <th class="report_name">Name</th>
+                    <th class="report_date">Date of report</th>
+                    <th class="report_title">Title</th>
+                    <th class="report_action">Action</th>
+                </tr>
+                <c:forEach var="report" items="${reports}" varStatus="status">
+                    <fmt:parseDate value="${report.reportDate}" pattern="yyyy-MM-dd" var="reportDay" type="date" />
+
+                    <tr class="row${status.count % 2}">
+                        <td class="report_name"><c:out value="${report.employee.name}" /></td>
+                        <td class="report_date"><fmt:formatDate value="${reportDay}" pattern="MM/dd/yyyy" /></td>
+                        <td class="report_title">${report.title}</td>
+                        <td class="report_action"><a href="<c:url value='?action=${actRep}&command=${commShow}&id=${report.id }' />">Show detail</a>
+                    </tr>
+                </c:forEach>
+            </tbody>
+        </table>
+
+        <div id="pagination">
+            (All ${employees_count} items)<br />
+            <c:forEach var="i" begin="1" end="${((employees_count - 1) / maxRow) + 1 }" step="1">
+                <c:choose>
+                    <c:when test="${i == page}">
+                        <c:out value="${i}" />&nbsp;
+                    </c:when>
+                    <c:otherwise>
+                        <a href="<c:url value='?action=${actRep}&command=${commIdx}&page=${i}' />"><c:out value="${i}" /></a>&nbsp;
+                    </c:otherwise>
+                </c:choose>
+            </c:forEach>
+        </div>
+        <p><a href="<c:url value='?action=${actRep}&command=${commNew}' />">Register a new daily report</a></p>
+    </c:param>
+</c:import>
